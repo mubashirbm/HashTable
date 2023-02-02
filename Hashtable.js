4786fdae15354c7cc3800ac6cc14304e321be184
mubashirbm <104245914+mubashirbm@users.noreply.github.com>
2023-02-02 18:01:01 +0530

    complete

--- Hashtable.js
+++ Hashtable.js
@@ -1,7 +1,44 @@
 
+
+        // Hashtable
+
 class Hash{
     constructor(size){
         this.Hashtable=new Array(size)
         this.size=size
     }
+    hash(key){
+        let total=0
+        for(let i=0;i<key.length;i++){
+            total += key.charCodeAt(i)
+        }
+        return total % this.size
+    }
+    set(key,value){
+        const index=this.hash(key)
+        this.Hashtable[index]=value
+    }
+    get(key){
+        const index=this.hash(key)
+        return this.Hashtable[index]
+    }
+    remove(key){
+        const index=this.hash(key)
+        return this.Hashtable[index]=undefined
+    }
+    display(){
+        for(let i=0;i<this.Hashtable.length;i++){
+            if(this.Hashtable[i]){
+                console.log(i,this.Hashtable[i])
+            }
+        }
+    }
 }
+const Hashtable=new Hash(20)
+Hashtable.set("name","mubashir")
+Hashtable.set("nae","ubashir")
+Hashtable.set("nam","mashir")
+console.log(Hashtable.get("name"))
+Hashtable.remove("name")
+Hashtable.display()
+// console.log(Hashtable)
